830b0a3a8c445790b18b6c5dcfbfcd743240753c
Сергей Отенов <serg10.otenov@yandex.ru>
2022-06-02 10:43:55 +0300

    Добавьте файлы проекта.

---
+++ Home_Work_11(1)/Consultant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_11_1_
+{
+    class Consultant
+    {
+        public void View(Client c)
+        {
+            string dataClient = $"{c.Surname} {c.FName} {c.LName} {c.TelephoneNumber} {c.pasport}";
+        }
+    }
+}
---
+++ Home_Work_11(1)/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Home_Work_11_1_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var consultant = new Consultant();
+            Client c = new Client("Отенов", "Сергей", "Александрович", "89859999999", 10, 12);
+            ListClients.Items.Add(c);
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
