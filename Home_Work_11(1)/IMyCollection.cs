4a41b364b61f1507fc658ea546e3634f3af9c3b5
Сергей Отенов <serg10.otenov@yandex.ru>
2023-01-19 01:12:58 +0300

    Изменена архитектура Добавлен абстрактный класс Worker От Worker наследуются остальные классы Добавлен интерфейс IMyCollection в котором коллекция для классов работников

---
+++ Home_Work_11(1)/IMyCollection.cs
@@ -0,0 +1,17 @@
+﻿using CreateFile;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_11_1_
+{
+    interface IMyCollection
+    {
+        ObservableCollection<Client> WorkerCollection { get; set; }
+
+
+    }
+}
--- Home_Work_11(1)/Manager.cs
+++ Home_Work_11(1)/Manager.cs
@@ -9,108 +9,24 @@ using CreateFile;
 
 namespace Home_Work_11_1_
 {
-    public class Manager : Consultant
+    public class Manager : Worker, IMyCollection
     {
+        public ObservableCollection<Client> WorkerCollection { get; set; }
+
         /// <summary>
         /// Конструктор. Создаёт менеджера. Менеджер работает с исходной коллекцией
         /// </summary>
         /// <param name="managerWindow">Окно, в котором работает менеджер</param>
         /// <param name="Name">Имя менеджера</param>
         /// <param name="clients">Исходная коллекция</param>
-        public Manager(ManagerWindow managerWindow, string Name, ObservableCollection<Client> clients) : base()
+        public Manager(ManagerWindow managerWindow, string name, ObservableCollection<Client> clients) : base(name, clients)
         {
-            this.clients = clients;
-            managerWindow.lw.ItemsSource = this.clients;
+            WorkerCollection = CopyCollection(clients);
+            managerWindow.lw.ItemsSource = WorkerCollection;
             managerWindow.lw.Visibility = Visibility.Hidden;
             managerWindow.btnSave.IsEnabled = false;
             managerWindow.btnChange.IsEnabled = false;
             managerWindow.txt.IsEnabled = false;
-        }
-
-        /// <summary>
-        /// Метод для кнопки изменить. Переопределён
-        /// </summary>
-        /// <param name="window"></param>
-        public override void Changed(Window window)
-        {
-            if (window is ManagerWindow)
-            {
-                ManagerWindow managerWindow = window as ManagerWindow;
-                string number = managerWindow.txt.Text;
-                if (String.IsNullOrEmpty(number) || number.Length != 11)
-                {
-                    MessageBox.Show("Вы ввели неверный номер телефона\nПопробуйте еще раз", "", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    managerWindow.txt.Text = default;
-                    managerWindow.txt.Focus();
-                    managerWindow.txt.ToolTip = "Был введён некорректный номер";
-                }
-                else
-                {
-                    ((Client)managerWindow.lw.SelectedItem).TelephoneNumber = managerWindow.txt.Text;
-                    MessageBox.Show("Телефонный номер сохранён", "", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Метод для подтаскивания данных в поля и отображения кнопок при выборе экземпляра списка. Переопределён
-        /// </summary>
-        /// <param name="window"></param>
-        public override void SelectionChangedMethod(Window window)
-        {
-            if (window is ManagerWindow)
-            {
-                ManagerWindow managerWindow = window as ManagerWindow;
-                managerWindow.txt.IsEnabled = true;
-                managerWindow.txt.Text = ((Client)managerWindow.lw.SelectedItem).TelephoneNumber;
-                managerWindow.btnSave.IsEnabled = true;
-                managerWindow.btnChange.IsEnabled = true;
-            }
-        }
-
-        /// <summary>
-        /// Метод для кнопки просмотр. Переопределён
-        /// </summary>
-        /// <param name="window"></param>
-        public override void View(Window window)
-        {
-            if (window is ManagerWindow)
-            {
-                ManagerWindow managerWindow = window as ManagerWindow;
-                if (managerWindow.lw.Visibility == Visibility.Visible) return;
-                managerWindow.lw.Visibility = Visibility.Visible;
-                if (!(managerWindow.lw.SelectedItem is null))
-                {
-                    managerWindow.txt.IsEnabled = true;
-                    managerWindow.btnSave.IsEnabled = true;
-                    managerWindow.btnChange.IsEnabled = true;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Метод для кнопки скрыть. Переопределён
-        /// </summary>
-        /// <param name="windoww"></param>
-        public override void Hide(Window window)
-        {
-            if (window is ManagerWindow)
-            {
-                ManagerWindow managerWindow = window as ManagerWindow;
-                if (managerWindow.lw.Visibility == Visibility.Hidden) return;
-                managerWindow.lw.Visibility = Visibility.Hidden;
-                managerWindow.btnSave.IsEnabled = false;
-                managerWindow.btnChange.IsEnabled = false;
-                managerWindow.txt.IsEnabled = false;
-            }
-        }
-        
-        /// <summary>
-        /// Сохранение всех изменений в файл
-        /// </summary>
-        public override void Save()
-        {
-            App.repositoryClients.SerializeClientsList(clients);
         }
 
         /// <summary>
@@ -137,11 +53,92 @@ namespace Home_Work_11_1_
         {
             Client newClient = new Client(addNewClientwWindow.surnameBox.Text, addNewClientwWindow.fnameBox.Text, addNewClientwWindow.lnameBox.Text, addNewClientwWindow.numberBox.Text, addNewClientwWindow.passportBox.Text);
 
-            clients.Add(newClient);
+            WorkerCollection.Add(newClient);
 
         }
-       
 
 
+        public override void Changed(Window window)
+        {
+            if (window is ManagerWindow)
+            {
+                ManagerWindow managerWindow = window as ManagerWindow;
+                string number = managerWindow.txt.Text;
+                if (String.IsNullOrEmpty(number) || number.Length != 11)
+                {
+                    MessageBox.Show("Вы ввели неверный номер телефона\nПопробуйте еще раз", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    managerWindow.txt.Text = default;
+                    managerWindow.txt.Focus();
+                    managerWindow.txt.ToolTip = "Был введён некорректный номер";
+                }
+                else
+                {
+                    ((Client)managerWindow.lw.SelectedItem).TelephoneNumber = managerWindow.txt.Text;
+                    MessageBox.Show("Телефонный номер сохранён", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+
+        public override void SelectionChangedMethod(Window window)
+        {
+            if (window is ManagerWindow)
+            {
+                ManagerWindow managerWindow = window as ManagerWindow;
+                managerWindow.txt.IsEnabled = true;
+                managerWindow.txt.Text = ((Client)managerWindow.lw.SelectedItem).TelephoneNumber;
+                managerWindow.btnSave.IsEnabled = true;
+                managerWindow.btnChange.IsEnabled = true;
+            }
+        }
+
+        public override void View(Window window)
+        {
+            if (window is ManagerWindow)
+            {
+                ManagerWindow managerWindow = window as ManagerWindow;
+                if (managerWindow.lw.Visibility == Visibility.Visible) return;
+                managerWindow.lw.Visibility = Visibility.Visible;
+                if (!(managerWindow.lw.SelectedItem is null))
+                {
+                    managerWindow.txt.IsEnabled = true;
+                    managerWindow.btnSave.IsEnabled = true;
+                    managerWindow.btnChange.IsEnabled = true;
+                }
+            }
+        }
+
+        public override void Hide(Window window)
+        {
+            if (window is ManagerWindow)
+            {
+                ManagerWindow managerWindow = window as ManagerWindow;
+                if (managerWindow.lw.Visibility == Visibility.Hidden) return;
+                managerWindow.lw.Visibility = Visibility.Hidden;
+                managerWindow.btnSave.IsEnabled = false;
+                managerWindow.btnChange.IsEnabled = false;
+                managerWindow.txt.IsEnabled = false;
+            }
+        }
+
+        public override void Back(Window window)
+        {
+            Save(WorkerCollection);
+            StartWindow startWindow = new StartWindow();
+            startWindow.Show();
+            window.Close();
+        }
+
+        public override void Sync(ObservableCollection<Client> clients)
+        {
+            for (int i = 0; i <= clients.Count - 1; i++)
+            {
+                
+                if (i > base.clients.Count-1 && i <= clients.Count-1)
+                {
+                    base.clients.Add(clients[i]);
+                }
+                else { base.clients[i] = clients[i]; }
+            }
+        }
     }
 }
