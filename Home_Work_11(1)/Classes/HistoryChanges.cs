5fb7e18b2609998eaccf23b51636ba4ce8fc6ca5
Сергей Отенов <serg10.otenov@yandex.ru>
2023-11-09 23:58:08 +0300

    Добавлены классы для отслеживания истории изменений

---
+++ Home_Work_11(1)/Classes/HistoryChanges.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_11_1_
+{
+    public struct HistoryChanges
+    {
+        ObservableCollection<HistoryRecord> historyChanges;
+    }
+}
+
