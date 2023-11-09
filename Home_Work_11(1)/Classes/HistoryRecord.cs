9035568b78360a0fd3cf2d707ba374ba3cb9722f
Сергей Отенов <serg10.otenov@yandex.ru>
2023-11-09 23:44:50 +0300

    Добавлены классы для отслеживания истории изменений

---
+++ Home_Work_11(1)/Classes/HistoryRecord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateFile
+{
+    public struct HistoryRecord
+    {
+        public DateTime DateOfHistory { get; set; }
+
+        private string author;
+
+        public string Author
+        {
+            get => author;
+            set
+            {
+                author = value;
+            }
+        }
+
+        public List<Record> Records;
+
+    }
+
+    public struct Record
+    {
+        public enum TypeOfChange
+        {
+            Change,
+            Add,
+            Delete
+        }
+
+        public string margin;
+
+        public string previousValue;
+
+        public string newValue;
+    }
+}
+
+
